77e7e918bf13b984795311632891d1a9a97734c5
Claudio Matos <pathcau16@gmail.com>
2020-04-27 21:38:27 -0300

    Iniciando Integração com backend

--- app/controller.js
+++ app/controller.js
@@ -4,21 +4,50 @@
 
     var spaApp = angular.module("spaApp");
     spaApp.controller('produtoController', produtoController);
-    produtoController.$inject = ['$scope'];
+    produtoController.$inject = ['$scope','$http'];
 
-    function produtoController($scope) {
+    function produtoController($scope,$http) {
         var vm = this;
         vm.produto = {};
         vm.produtos = [];
+
+        vm.init= function(){
+            vm.listarProdutos()
+        };
+
+        vm.listarProdutos = function(){
+            $http.get('http://localhost:3000/produtos').then(
+                function(response){
+                    console.log(response);
+                    vm.produtos = response.data;
+                }, 
+                function(error){
+
+                }
+            )
+        };
 
         vm.adicionarProduto = function () {
             if(vm.produto.id != undefined){
                 vm.atualizarItem(vm.produto, vm.produto.id);
                 vm.limparCampos();
             }else{
-                vm.produtos.push(angular.copy(vm.produto));
-                vm.limparCampos();
+              //  vm.produtos.push(angular.copy(vm.produto));
+               // vm.limparCampos();
+               vm.salvarProdutoBaseDados();
             }
+        };
+        vm.salvarProdutoBaseDados = function(){
+            $http.post('http://localhost:3000/produtos',vm.produto).then(
+                function(response){
+                    console.log(response);
+                    vm.produtos.push(vm.produto);
+                    vm.limparCampos();
+                }, 
+                function(error){
+
+                }
+            )
         };
 
         vm.limparCampos = function () {
