46efb1f90f762022cf4c8aeea871540200a0b27b
Claudio Matos <pathcau16@gmail.com>
2020-04-30 00:07:48 -0300

    Ediçao e exclusao

--- app/controller.js
+++ app/controller.js
@@ -4,72 +4,86 @@
 
     var spaApp = angular.module("spaApp");
     spaApp.controller('produtoController', produtoController);
-    produtoController.$inject = ['$scope','$http'];
+    produtoController.$inject = ['$scope', '$http'];
 
-    function produtoController($scope,$http) {
+    function produtoController($scope, $http) {
         var vm = this;
         vm.produto = {};
         vm.produtos = [];
+        var HTTP_HOST = 'http://localhost:3000';
 
-        vm.init= function(){
-            vm.listarProdutos()
+        vm.init = function () {
+            vm.listarProdutos();
         };
 
-        vm.listarProdutos = function(){
-            $http.get('http://localhost:3000/produtos').then(
-                function(response){
-                    console.log(response);
+        vm.listarProdutos = function () {
+            $http.get(HTTP_HOST + '/produtos').then(
+                function (response) {
                     vm.produtos = response.data;
-                }, 
-                function(error){
-
+                },
+                function (error) {
+                    alert('Erro ao recuperar produtos');
                 }
-            )
+            );
         };
 
         vm.adicionarProduto = function () {
-            if(vm.produto.id != undefined){
-                vm.atualizarItem(vm.produto, vm.produto.id);
+            if (vm.produto._id != undefined) {
+                vm.atualizarItem(vm.produto, vm.produto.index);
                 vm.limparCampos();
-            }else{
-              //  vm.produtos.push(angular.copy(vm.produto));
-               // vm.limparCampos();
-               vm.salvarProdutoBaseDados();
+            } else {
+                //  vm.produtos.push(angular.copy(vm.produto));
+                // vm.limparCampos();
+                vm.salvarProdutoBaseDados();
             }
         };
-        vm.salvarProdutoBaseDados = function(){
-            $http.post('http://localhost:3000/produtos',vm.produto).then(
-                function(response){
-                    console.log(response);
-                    vm.produtos.push(vm.produto);
+        vm.salvarProdutoBaseDados = function () {
+            $http.post(HTTP_HOST + '/produtos', vm.produto).then(
+                function (response) {
+                    vm.produtos.push(response.data.produtoCriado);
                     vm.limparCampos();
-                }, 
-                function(error){
-
+                },
+                function (error) {
+                    alert('Erro ao inserir produto');
                 }
-            )
+            );
         };
 
         vm.limparCampos = function () {
             vm.produto = {};
         };
 
-        vm.deletarItem = function(index){
-            vm.produtos.splice(index, 1);
+        vm.deletarItem = function (item, index) {
+            $http.delete(HTTP_HOST+'/produtos/'+ item._id).then(
+                function(response){
+                    vm.produtos.splice(index, 1);
+                    vm.limparCampos();
+                }, 
+                function(error){
+                    alert('Erro ao deletar produto');
+                }
+            );
         };
 
-        vm.carregarItem = function(index){
+        vm.carregarItem = function (item, index) {
             vm.produto = {
-                id: index,
-                nome: vm.produtos[index].nome,
-                preco: vm.produtos[index].preco
+                _id: item._id,
+                nome: item.nome,
+                preco: item.preco,
+                index: index
             };
         };
 
-        vm.atualizarItem = function(item, index){   
-            if(index === item.id){
-                vm.produtos[index] = item;
-            }
+        vm.atualizarItem = function (item, index) {
+            $http.put(HTTP_HOST + '/produtos/' + item._id, item).then(
+                function (response) {
+                    vm.produtos[index] = item;
+                    vm.limparCampos();
+                },
+                function (error) {
+                    alert('Erro ao atualizar produto');
+                }
+            );
         };
     }
 }());
